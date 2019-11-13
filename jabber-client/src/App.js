b3f8c5c9e91c132660c7f9ca299d258d4c02b4fe
Michael <michaeltroya13@gmail.com>
2019-11-13 11:39:19 -0500

    initial commit

---
+++ jabber-client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+export default function App() {
+  return (
+    <div>
+      <h1>hi</h1>
+      <h2>world</h2>
+    </div>
+  );
+}
