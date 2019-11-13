bd27fc93b42b6959ffd0b9d79511f3b61ea2d364
Michael <michaeltroya13@gmail.com>
2019-11-13 12:07:19 -0500

    finish server setup

--- jabber-server/index.js
+++ jabber-server/index.js
@@ -4,8 +4,20 @@ const http = require('http');
 
 const PORT = process.env.PORT || 5000;
 
+const router = require('./router');
+
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
+
+io.on('connection', socket => {
+  console.log('New connection!');
+
+  socket.on('disconnect', () => {
+    console.log('Connection closed!');
+  });
+});
+
+app.use(router);
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
