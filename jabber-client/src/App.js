2ce8a75fb03a553475b2c8f589f99c8a5fe7d625
Michael <michaeltroya13@gmail.com>
2019-11-13 11:59:46 -0500

    initialize http server

--- jabber-client/src/App.js
+++ jabber-client/src/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-export default function App() {
-  return (
-    <div>
-      <h1>hi</h1>
-      <h2>world</h2>
-    </div>
-  );
-}
+import Join from './components/Join';
+import Chat from './components/Chat';
+
+const App = () => (
+  <Router>
+    <Route path="/" exact component={Join} />
+    <Route path="/chat" component={Chat} />
+  </Router>
+);
+
+export default App;
